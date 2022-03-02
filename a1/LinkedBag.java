7eb0c1f8adb6d47e9a3bf6d4c4bb752bbd7b7e6e
BTanjerine <40650058+BTanjerine@users.noreply.github.com>
2022-03-02 02:32:14 -0800

    files for assignment 1 (incomplete)

---
+++ a1/LinkedBag.java
@@ -0,0 +1,210 @@
+/*
+Brayden Tanji 
+February 24, 2022
+CS 2400
+bag Data Structure class linked chain version
+*/
+
+public class LinkedBag<T> implements BagInterface<T>{
+	private Node<T> firstNode;
+	private int numberOfEntries;
+
+
+	/*
+	return: integer
+	input: n/a 
+	get the size of the bag (items it is holding)
+	*/
+	public int getCurrentSize(){
+		return numberOfEntries;
+	}
+
+	/*
+	return: boolean
+	input: n/a
+	check if the bag is empty 
+	*/
+	public boolean isEmpty(){
+		return numberOfEntries == 0;
+	}
+
+	/*
+	return: boolean
+	input: T generic
+	add an item to the bag
+	*/
+	public boolean add(T newEntry){
+		Node<T> newNode = new Node<T>(newEntry);	// make a new node with given value and link new node to the chain
+		newNode.setNextNode(firstNode);
+
+		firstNode = newNode;				// make the first node the new node
+
+		numberOfEntries++;				//increment number of entries
+
+		return true;
+	}
+
+	/*
+	return: T generic 
+	input: n/a
+	removes a random item from the list
+	*/
+	public T remove(){
+		T result = firstNode.getData();
+		if(getCurrentSize() > 0 || firstNode != null){		// check if array is empty to make sure that there is a removable entry 
+			firstNode = firstNode.getNextNode();
+		}
+
+		numberOfEntries--;					// decrement number of entries
+		return result;						// return value of removed data
+	}
+
+	/*
+	return: T generic 
+	input: T generic Entry to remove
+	removes a item from the list
+	*/
+	public T remove(T entryToRemove){
+		Node<T> nodeToRemove = getReferenceTo(entryToRemove);	// find the node we need to remove
+
+		nodeToRemove.setData(firstNode.getData());		// set that node to the first node's value
+
+		remove();						// remove the first node
+
+		return entryToRemove;
+	}
+
+
+	/*
+	return: Node 
+	input: T generic entry you are looking for
+	finds the location of the item in the bag linked chain
+	*/
+	private Node<T> getReferenceTo(T entry){
+		Node<T> currentNode = firstNode;			// start with first node
+		for(int i=0; i<numberOfEntries; i++){ 			// loop till the current node has the data we are looking for
+			if(currentNode.getData() == entry){
+				break;
+			}
+			currentNode = currentNode.getNextNode();	// if not move to the next node
+		}
+		return currentNode;					// return node with same value as one being searched
+	}
+
+	/*
+	return: n/a
+	input: n/a
+	empties the elements of the bag
+	*/
+	public void clear(){
+		firstNode = null;					// set first node null == all nodes deleted
+		numberOfEntries = 0;					// reset entry value
+	}
+
+	/*
+	return: T generic
+	input: int index position
+	get the specific entry at the psoiton given in the bag
+	*/
+	public T getEntry(int index){
+		Node<T> currentNode = firstNode;			// start with first node
+		for(int i=0; i<index; i++){ 				// loop till we reach the correct node
+			currentNode = currentNode.getNextNode();	
+		}
+
+		return currentNode.getData();				// return that data
+	}
+
+	/*
+	return: integer
+	input: T generic
+	return the number similar items there are in the bag
+	*/
+	public int getFrequencyOf(T entry){
+		Node<T> currentNode = firstNode;			// start with first node
+		int count = 0;
+
+		for(int i=0; i<numberOfEntries; i++){ 			// loop till the current node has the data we are looking for
+			if(currentNode.getData() == entry){
+				count++;				// increment count ofthat item
+			}
+			currentNode = currentNode.getNextNode();	// move to the next node
+		}
+
+		return count;						// return frequency of that entry
+	}
+
+	/*
+	return: boolean
+	input: T generic
+	check if a specific item exists in the bag
+	*/
+	public boolean contains(T entry){
+		Node<T> currentNode = firstNode;			// start with first node
+		boolean found = false;
+
+		for(int i=0; i<numberOfEntries; i++){ 			// loop till the current node has the data we are looking for
+			found = currentNode.getData() == entry;
+			if(found)
+				break;					
+
+			currentNode = currentNode.getNextNode();	// move to the next node
+		}
+
+		return found;
+	}
+
+	/*
+	return: T array
+	input: n/a
+	return an array of the bag 
+	*/
+	public T[] toArray(){
+		@SuppressWarnings("unchecked")
+		T[] newArray = (T[]) new Object[numberOfEntries];
+
+		Node<T> currentNode = firstNode;			// start with first node
+
+		for(int i=0; i<numberOfEntries; i++){ 			// loop till the current node has the data we are looking for
+			
+			newArray[i] = currentNode.getData();		// copy values of chain to array
+
+			currentNode = currentNode.getNextNode();	// move to the next node
+		}
+
+		return newArray;
+	}
+
+	/*
+	return: bag (of any type)
+	input: bag (of any type)
+	return a new bag with the elements of both bags combined together
+	*/
+	public BagInterface<T> union(BagInterface<T> bag1){
+		LinkedBag<T> newBag = new LinkedBag<T>();
+
+		return newBag;
+	}
+
+	/*
+	return: bag (of any type)
+	input: bag (of any type)
+	return a new bag with elements that are similar across both bags
+	*/
+	public BagInterface<T> intersection(BagInterface<T> bag1){
+		LinkedBag<T> newBag = new LinkedBag<T>();
+
+		return newBag;
+	}
+
+	/*
+	return: bag (of any type)
+	input: bag (of any type)
+	return a new bag with similar elements from both bags subtracted
+	*/
+	public BagInterface<T> difference(BagInterface<T> bag1){
+		LinkedBag<T> newBag = new LinkedBag<T>();
+
+		return newBag;
+	}
+}
---
+++ a1/ResizeableArrayBag.java
@@ -0,0 +1,257 @@
+import java.util.Arrays;
+
+/*
+Brayden Tanji 
+February 24, 2022
+CS 2400
+bag Data Structure class array version
+*/
+
+public class ResizeableArrayBag<T> implements BagInterface<T>{
+	private final T[] bag;
+	private int numberOfEntries;
+	private static final int DEFAULT_CAPACITY = 25;
+
+
+	/*
+	input: integer capacity
+	make a new bag array with a custom capacity
+	*/
+	public ResizeableArrayBag(int capacity){
+		numberOfEntries = 0;
+
+		@SuppressWarnings("unchecked")
+		T[] tempbag = (T[]) new Object[capacity];
+		bag = tempbag;
+	}
+
+	/*
+	input: n/a
+	make a new bag array with a capacity of 25
+	*/
+	public ResizeableArrayBag(){
+		numberOfEntries = 0;
+
+		@SuppressWarnings("unchecked")
+		T[] tempbag = (T[]) new Object[DEFAULT_CAPACITY];
+		bag = tempbag;
+	}
+
+	/*
+	return: integer
+	input: n/a 
+	get the size of the bag (items it is holding)
+	*/
+	public int getCurrentSize(){
+		return numberOfEntries;
+	}
+
+	/*
+	return: boolean
+	input: n/a
+	check if the bag is empty (true/false)
+	*/
+	public boolean isEmpty(){
+		return numberOfEntries == 0;
+	}
+
+	/*
+	return: boolean
+	input: T generic
+	add an item to the bag
+	*/
+	public boolean add(T newEntry){
+		if(isFull()){
+			doubleSize(bag);		// increase size of bag when the item doesn't fit in the array
+		}
+
+		bag[numberOfEntries] = newEntry;	// add entry
+
+		numberOfEntries++;			// increase count
+		return true;
+	}
+
+	/*
+	return: T array 
+	input: T array
+	doubles the size of the bag
+	*/
+	private T[] doubleSize(T[] oldArray){
+		return Arrays.copyOf(oldArray, oldArray.length*2);
+	}
+
+	/*
+	return: boolean 
+	input: n/a
+	check if the bag is full (true/false)
+	*/
+	private boolean isFull(){
+		return numberOfEntries == bag.length;
+	}
+
+	/*
+	return: T generic 
+	input: n/a
+	removes a random item from the list
+	*/
+	public T remove(){
+
+		return removeEntry(numberOfEntries);
+	}
+
+	/*
+	return: T generic 
+	input: T generic Entry to remove
+	removes a item from the list
+	*/
+	public T remove(T entryToRemove){
+		int indexToRemove = getIndexOf(entryToRemove);		// find the index of the entry to remove
+		return removeEntry(indexToRemove);			// remove entry and return the old value
+	}
+
+	/*
+	return: int array index 
+	input: T generic entry you are looking for
+	finds the location of the item in the bag
+	*/
+	private int getIndexOf(T entry){
+		int i = 0;
+
+		while(bag[i] != entry && i < numberOfEntries){		// loop till entry is found in the bag array
+			i++;
+		}
+
+		return i;
+	}
+
+	/*
+	return: T generic entry you are looking for 
+	input: int array index
+	Removes a specific entry from the bag
+	*/
+	private T removeEntry(int index){
+		T output = null;
+
+		if(!isEmpty() && index != 0){
+			output = bag[index];
+			bag[index] = bag[numberOfEntries-1]; 	// move last entry to the area
+			bag[numberOfEntries-1] = null;		// clear the last entry
+			numberOfEntries--;			// decrease number of entries
+		}
+
+		return output;
+	}
+
+	/*
+	return: void
+	input: n/a
+	empties the elements of the bag
+	*/
+	public void clear(){
+		numberOfEntries = 0;
+	}
+
+	/*
+	return: T generic
+	input: int index position
+	get the specific entry at the psoiton given in the bag
+	*/
+	public T getEntry(int index){
+		return bag[index];
+	}
+
+	/*
+	return: integer
+	input: T generic
+	return the number similar items there are in the bag
+	*/
+	public int getFrequencyOf(T entry){
+		int i = 0;
+		int count = 0;
+
+		while(i < numberOfEntries){		// loop till the whole bag is checked
+			if(bag[i] == entry)
+				count++;		// count up the amount of similar items there are
+			i++;
+		}
+
+		return count;				// return the number of similar items in the bag
+	}
+
+	/*
+	return: boolean
+	input: T generic
+	check if a specific item exists in the bag
+	*/
+	public boolean contains(T entry){
+		int i = 0;
+		boolean found = false;
+
+		while(i < numberOfEntries && !found){		// loop till item is found
+			found = (bag[i] == entry);		// check the existence of the item in the bag
+			i++;
+		}
+
+		return found;					// return the existence of the entry
+	}
+
+	/*
+	return: T array
+	input: n/a
+	return a new array of the bag 
+	*/
+	public T[] toArray(){
+		@SuppressWarnings("unchecked")				// make a new array to represent bag
+		T[] finalArray = (T[]) new Object[numberOfEntries];
+
+		for(int i=0; i<finalArray.length; i++){			// copy all of the items in bag to the new array
+			finalArray[i] = bag[i];
+		}
+
+		return finalArray;					// return the array
+	}
+
+	/*
+	return: resizeableArrayBagBag 
+	input: resizeableArrayBagBag
+	return a new bag with the elements of both bags combined together
+	*/
+	public BagInterface<T> union(BagInterface<T> bag1){
+		ResizeableArrayBag<T> newBag = new ResizeableArrayBag<T>();
+
+		for(int i=0; i<numberOfEntries+bag1.getCurrentSize(); i++){	// loop till all the elements are added into the array
+			if(i < numberOfEntries)					// switch the bag you are adding from based on number of entries for this bag
+				newBag.add(bag[i]);
+			else if(i >= numberOfEntries)				// adding items from bag1
+				newBag.add(bag1.getEntry(i));
+		}
+
+		return newBag;
+	}
+
+	/*
+	return: resizeableArrayBagBag 
+	input: resizeableArrayBagBag
+	return a new bag with elements that are similar across both bags
+	*/
+	public BagInterface<T> intersection(BagInterface<T> bag1){
+		ResizeableArrayBag<T> newBag = new ResizeableArrayBag<T>();
+
+		
+
+		return newBag;
+	}
+
+	/*
+	return: resizeableArrayBagBag 
+	input: resizeableArrayBagBag
+	return a new bag with similar elements from both bags subtracted
+	*/
+	public BagInterface<T> difference(BagInterface<T> bag1){
+		ResizeableArrayBag<T> newBag = new ResizeableArrayBag<T>();
+
+
+
+		return newBag;
+	}
+}
